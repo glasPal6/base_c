84975eabc0e67787b8e758e0836fbc5a33448192
glaspal6 <dylan.r.kamstra@gmail.com>
2025-03-13 20:05:03 +0200

    feat(da): Added find item

--- macros/dynamic_array.h
+++ macros/dynamic_array.h
@@ -19,7 +19,7 @@
 #define da_get(da, index) ((index) < (da)->count ? (da)->items[index] : 0)
 
 #define da_append(da, item)                                                    \
-    do {                                                                       \
+    ({                                                                         \
         if ((da)->count >= (da)->capacity) {                                   \
             (da)->capacity =                                                   \
                 (da)->capacity == 0 ? DA_INIT_CAP : (da)->capacity * 2;        \
@@ -28,10 +28,10 @@
             assert((da)->items != NULL && "Realloc failed");                   \
         }                                                                      \
         (da)->items[(da)->count++] = (item);                                   \
-    } while (0)
+    })
 
 #define da_append_many(da, new_items, new_items_count)                         \
-    do {                                                                       \
+    ({                                                                         \
         if ((da)->count + (new_items_count) > (da)->capacity) {                \
             while ((da)->count + (new_items_count) > (da)->capacity) {         \
                 (da)->capacity =                                               \
@@ -44,18 +44,18 @@
         memcpy((da)->items + (da)->count, new_items,                           \
                (new_items_count) * sizeof(*(da)->items));                      \
         (da)->count += (new_items_count);                                      \
-    } while (0)
+    })
 
 #define da_remove(da, index)                                                   \
-    do {                                                                       \
+    ({                                                                         \
         assert((index) < (da)->count && "Index out of bounds");                \
         memmove((da)->items + (index), (da)->items + (index) + 1,              \
                 ((da)->count - (index) - 1) * sizeof(*(da)->items));           \
         (da)->count--;                                                         \
-    } while (0)
+    })
 
 #define da_remove_many(da, remove_start, remove_count)                         \
-    do {                                                                       \
+    ({                                                                         \
         assert((remove_start) + (remove_count) <= (da)->count &&               \
                "Range out of bounds");                                         \
         memmove((da)->items + (remove_start),                                  \
@@ -63,13 +63,13 @@
                 ((da)->count - (remove_start) - (remove_count)) *              \
                     sizeof(*(da)->items));                                     \
         (da)->count -= (remove_count);                                         \
-    } while (0)
+    })
 
 #define da_foreach(type, item, da)                                             \
     for (type *item = (da)->items; (item) < (da)->items + (da)->count; ++item)
 
 #define da_resize(da, new_size)                                                \
-    do {                                                                       \
+    ({                                                                         \
         if ((new_size) > (da)->capacity) {                                     \
             (da)->capacity = (new_size);                                       \
             (da)->items = NOB_REALLOC((da)->items,                             \
@@ -77,12 +77,25 @@
             NOB_ASSERT((da)->items != NULL && "Buy more RAM lol");             \
         }                                                                      \
         (da)->count = (new_size);                                              \
-    } while (0)
+    })
+
+#define da_find(da, item)                                                      \
+    ({                                                                         \
+        size_t index = 0;                                                      \
+        size_t result = (size_t)-1;                                            \
+        for (index = 0; index < (da)->count; ++index) {                        \
+            if ((da)->items[index] == (item)) {                                \
+                result = index;                                                \
+                break;                                                         \
+            }                                                                  \
+        }                                                                      \
+        result;                                                                \
+    })
 
 #define da_free(da)                                                            \
-    do {                                                                       \
+    ({                                                                         \
         free((da)->items);                                                     \
         (da)->items = NULL;                                                    \
         (da)->count = 0;                                                       \
         (da)->capacity = 0;                                                    \
-    } while (0)
+    })
